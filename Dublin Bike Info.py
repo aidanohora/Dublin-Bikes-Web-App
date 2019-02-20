864bc0d8ee024b7212bce291a1739264feeaab57
Abhishek Sinha <abhishek.sinha@ucdconnect.ie>
2019-02-20 19:04:53 +0000

    Changes Made
    New Changes

--- Dublin Bike Info.py
+++ Dublin Bike Info.py
@@ -8,16 +8,26 @@ timeout = 3600 # Sixty seconds
 def doWork():
     with open('Dublin Bike Info.txt', 'a') as outfile:
         data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
+        keep_keys = set()
+        for d in data:
+            for key, value in d.items():
+                if value is  True or value is False or key == 'bike_stands':
+                    keep_keys.add(key)
+        remove_keys =  keep_keys
+        print(remove_keys)
+        for d in data:
+            for k in remove_keys:
+                del d[k]
         print(data)
-        json.dump(data, outfile)
-    with open('Dublin Bike Info.cvs', 'a') as outfile:
-        data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        print(data)
-        json.dump(data, outfile)
-    with open('Dublin Bike Info.json', 'a') as outfile:
-        data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        print(data)
-        json.dump(data, outfile)
+        #json.dump(data, outfile)
+    #with open('Dublin Bike Info.cvs', 'a') as outfile:
+        #data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
+        #print(data)
+        #json.dump(data, outfile)
+    #with open('Dublin Bike Info.json', 'a') as outfile:
+        #data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
+        #print(data)
+        #json.dump(data, outfile)
 l = task.LoopingCall(doWork)
 l.start(timeout) # call every sixty seconds
 
--- Dublin weather info.py
+++ Dublin weather info.py
@@ -8,8 +8,18 @@ timeout = 3600 # Sixty seconds
 def doWork():
     with open('Weather Info.json', 'a') as outfile:
         data = requests.get("https://prodapi.metweb.ie/weather/short/Dublin").json()
+        del data['temperatureClass']
+        del data['symbol']
+        del data['windGust']
+        del data['pressure']
+        del data['canonicalWindDirection']
+        del data['highestWarning']
+        del data['weatherDescription']
+        del data['weather']
+        del data['windDirection']
+        del data['humidity']
         print(data)
-        json.dump(data, outfile)
+        #json.dump(data, outfile)
 
 l = task.LoopingCall(doWork)
 l.start(timeout) # call every sixty seconds
