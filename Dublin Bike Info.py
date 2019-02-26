0ac70fb65fa8da6f8c9170bb3da6bdab7ef207a4
Aidan <aidan.ohora@ucdconnect.ie>
2019-02-26 09:47:14 +0000

    To clone new repo

--- Dublin Bike Info.py
+++ Dublin Bike Info.py
@@ -1,47 +1,96 @@
 from twisted.internet import task, reactor
+from datetime import datetime
 import requests
 import json
 import os
-import csv
-timeout = 3600 # Sixty seconds
+import sqlite3
+import pymysql
+import sys
+
+timeout = 3600  # Sixty seconds
+
 
 def doWork():
     with open('Dublin Bike Info.txt', 'a') as outfile:
-        data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
+        data = requests.get(
+            "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
         keep_keys = set()
         for d in data:
             for key, value in d.items():
-                if value is  True or value is False:
+                if value is True or value is False:
                     keep_keys.add(key)
-        remove_keys =  keep_keys
+        remove_keys = keep_keys
         print(remove_keys)
         for d in data:
             for k in remove_keys:
                 del d[k]
-        print(data)
-#<<<<<<< HEAD
-        #json.dump(data, outfile)
-    #with open('Dublin Bike Info.cvs', 'a') as outfile:
-        #data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        #print(data)
-        #json.dump(data, outfile)
-    #with open('Dublin Bike Info.json', 'a') as outfile:
-        #data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        #print(data)
-        #json.dump(data, outfile)
-#=======
-    #    json.dump(data, outfile)
-    #with open('Dublin Bike Info.cvs', 'a') as outfile:
-    #    data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-    #    print(data)
-        #json.dump(data, outfile)
-    #with open('Dublin Bike Info.json', 'a') as outfile:
-    #data = requests.get("https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        #print(data)
-        #json.dump(data, outfile)
+        for d in data:
+            for key, value in d.items():
+                if key == "number":
+                    number = d[key]
+                    print("number=", number)
+                elif key == "contract_name":
+                    name = d[key]
+                    print("name=", name)
+                elif key == "address":
+                    address = d[key]
+                    address = address.replace("'", "`")
+                    # address = address.replace("(O'Connell's)", "(O`Connell`s)")
+                    # address = address.replace("Sir Patrick's Dun", "Sir Patrick`s Dun")
+                    # address = address.replace("St. Stephen's Green South", "St. Stephen`s Green South")
 
-#>>>>>>> aa75241ae12bc1476f940189e26057840015239d
+                    print("address=", address)
+                elif key == 'position':
+                    lat = d[key]['lat']
+                    long = d[key]['lng']
+                    print("lat=", lat, "long=", long)
+                elif key == 'bike_stands':
+                    bike_stands = d[key]
+                    print("bike_stands:", bike_stands)
+                elif key == 'status':
+                    status = d[key]
+                    print("status:", status)
+                elif key == 'available_bike_stands':
+                    available_bike_stands = d[key]
+                    print("available_bike_stands", available_bike_stands)
+                elif key == 'available_bikes':
+                    available_bikes = d[key]
+                    print("available_bikes =", available_bikes)
+                elif key == 'last_update':
+                    dt = d[key]
+                    dt = int(dt)
+                    dt = dt / 1000
+                    date = datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S')
+                    date, time = date.split(" ")
+                    print("date:", date)
+                    print("time:", time)
+                    # print(data)
+                    REGION = 'us-east-2'
+                    rds_host = 'dublin-bikes.c9vk2yiybuop.us-east-2.rds.amazonaws.com'
+                    print("inside 12")
+                    name1 = "root"
+                    password = 'database123'
+                    db_name = "dbbikes"
+                    id = 1
+                    conn = pymysql.connect(rds_host, user=name1, passwd=password, db=db_name, connect_timeout=5)
+                    with conn.cursor() as cur:
+                        print("inside 1")
+                        # cur.execute("""delete from station_fixed""")
+                        # cur.execute("""delete from station_var""")
+                        cur.execute(
+                            """insert into station_fixed (station_no, name, address ,latitude, longitude, bike_stands) values( %s, '%s' , '%s' , %s , %s, %s)""" % (
+                            number, name, address, lat, long, bike_stands))
+                        cur.execute(
+                            """insert into station_var (status, available_stands, available_bikes ,last_update_date, lat_update_time) values( '%s', %s , %s , '%s' , '%s')""" % (
+                            status, available_bike_stands, available_bikes, date, time))
+
+                        # cur.execute("""select * from weather""")
+                        print("inside 11")
+                        conn.commit()
+                        cur.close()
+
+
 l = task.LoopingCall(doWork)
-l.start(timeout) # call every sixty seconds
+l.start(timeout)  # call every sixty seconds
 
 reactor.run()
--- Dublin weather info.py
+++ Dublin weather info.py
@@ -1,13 +1,20 @@
 from twisted.internet import task, reactor
+from datetime import datetime
 import requests
 import json
 import os
 import sqlite3
-timeout = 0 # Sixty seconds
+import pymysql
+import sys
+
+timeout = 360000000  # Sixty seconds
 from datetime import datetime
+
+
 def doWork():
     with open('Weather Info.json', 'a') as outfile:
-        data = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=6fb76ecce41a85161d4c6ea5e2758f2b").json()
+        data = requests.get(
+            "http://api.openweathermap.org/data/2.5/weather?q=Dublin&appid=6fb76ecce41a85161d4c6ea5e2758f2b").json()
         del data['coord']
         del data['base']
         del data['visibility']
@@ -17,17 +24,47 @@ def doWork():
         del data['id']
         del data['name']
         del data['cod']
-        #print(data['weather'])
         for k in data['weather']:
-            for key,value in list(k.items()):
+            for key, value in list(k.items()):
                 if key == "id" or key == "icon":
                     del k[key]
+                elif key == "main":
+                    desc = k[key]
         del data['main']['pressure']
         del data['main']['humidity']
         del data['main']['temp_min']
         del data['main']['temp_max']
-        print(data)
-l = task.LoopingCall(doWork)
-l.start(timeout) # call every sixty seconds
+        dt = data['dt']
+        dt = int(dt)
+        date = datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S')
+        date, time = date.split(" ")
+        print(date)
+        print(time)
+        temp = data['main']['temp']
+        print(temp)
+        # print(data)
+        print(desc)
+        print("inside 1")
+        REGION = 'us-east-2'
+        rds_host = 'dublin-bikes.c9vk2yiybuop.us-east-2.rds.amazonaws.com'
+        print("inside 12")
+        name = "root"
+        password = 'database123'
+        db_name = "dbbikes"
+        id = 1
+        conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+        with conn.cursor() as cur:
+            print("inside 1")
+            # cur.execute("""delete from weather""")
+            cur.execute(
+                """insert into weather (temperature, description, time ,date) values( %s, '%s' , '%s' , '%s')""" % (
+                temp, desc, time, date))
+            # cur.execute("""select * from dbbikes.weather""")
+            print("inside 11")
+            conn.commit()
+            cur.close()
 
+
+l = task.LoopingCall(doWork)
+l.start(timeout)  # call every sixty seconds
 reactor.run()
