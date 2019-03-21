0fd96bb4fae00e83b104b35e501351e7e1918ec4
aidanohora <aidan.ohora@ucdconnect.ie>
2019-03-21 19:57:37 +0000

    Introduced weekday/weekend and time difference

--- nearest_neighbours.py
+++ nearest_neighbours.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import math
+import datetime
 
 mydb = mysql.connector.connect(
     host="dublin-bikes.c9vk2yiybuop.us-east-2.rds.amazonaws.com",
@@ -31,35 +32,82 @@ current_weather = weather_row[1]
 current_time = weather_row[2]
 #print("Current time:", current_time)
 
-for station_no in station_nos:
-    #cursor.execute("SELECT available_bikes FROM dbbikes.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
-    #current_bikes = cursor.fetchone()
-    #print("Current bikes at station no.", station_no, "is:", current_bikes)
-    cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 6 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s'" % (station_no, current_weather))
-    rows = cursor.fetchall()
-    #print(rows)
-    if rows == []:
-        #print("New weather encountered:", current_weather)
-        cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 6 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN'" % station_no)
+current_day = weather_row[3].weekday()
+
+
+#print(current_day)
+
+#if weekday, use only weekday records:
+if (current_day < 5):
+    for station_no in station_nos:
+        #cursor.execute("SELECT available_bikes FROM dbbikes.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
+        #current_bikes = cursor.fetchone()
+        #print("Current bikes at station no.", station_no, "is:", current_bikes)
+        cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) < 5" % (station_no, current_weather))
         rows = cursor.fetchall()
-    weight_total = 0 
-    weighted_bikes_total = 0  
-    for row in rows:
-        row_temp = row[6]
-        #print("Temp of row:", row_temp)
-        row_bikes = row[2]
-        #print("Available bikes for this row:", row_bikes)
-        weight = 1/(math.sqrt((row_temp - current_temp)**2) + 1)
-        #print("Weight:", weight)
-        weight_total += weight
-        #print("Total of weights:", weight_total)
-        weighted_bikes = row_bikes * weight
-        #print("Available bikes (weighted):", weighted_bikes)
-        weighted_bikes_total += weighted_bikes
-        #print("Weighted bike total so far:", weighted_bikes_total)
-    prediction = round(weighted_bikes_total/weight_total)
-    print("Available bikes at station", station_no, "prediction:", prediction)
-print("Done.")
+        #print(rows)
+        if rows == []: #if a new weather is encountered, use records for all weather
+            #print("New weather encountered:", current_weather)
+            cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' weekday(weather.date) < 5" % station_no)
+            rows = cursor.fetchall()
+        weight_total = 0 
+        weighted_predictors_total = 0  
+        for row in rows:
+            row_temp = row[6]
+            #print("Temp of row:", row_temp)
+            row_bikes = row[2]
+            #print("Available bikes for this row:", row_bikes)
+            row_time = row[8]
+            #print("Time in row: ", row_time)
+            temp_weight = 1/(math.sqrt((row_temp - current_temp)**2) + 0.5)
+            time_weight = 1/(math.sqrt((round((row_time - current_time).total_seconds()/60)**2)) + 0.5)
+            weight = temp_weight + time_weight 
+            #print("Weight:", weight)
+            weight_total += weight
+            #print("Total of weights:", weight_total)
+            weighted_predictor = row_bikes * weight
+            #print("Available bikes (weighted):", weighted_bikes)
+            weighted_predictors_total += weighted_predictor
+            #print("Weighted bike total so far:", weighted_bikes_total)
+        prediction = round(weighted_predictors_total/weight_total)
+        print("Available bikes at station", station_no, "prediction:", prediction)
+    print("Done.")
+
+#if weekend, use only records from that day:
+else:
+    for station_no in station_nos:
+        #cursor.execute("SELECT available_bikes FROM dbbikes.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
+        #current_bikes = cursor.fetchone()
+        #print("Current bikes at station no.", station_no, "is:", current_bikes)
+        cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) = %s" % (station_no, current_weather, current_day))
+        rows = cursor.fetchall()
+        #print(rows)
+        if rows == []: #if a new weather is encountered, use records for all weather
+            #print("New weather encountered:", current_weather)
+            cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weekday(weather.date) = %s" % (station_no, current_day))
+            rows = cursor.fetchall()
+        weight_total = 0 
+        weighted_predictors_total = 0  
+        for row in rows:
+            row_temp = row[6]
+            #print("Temp of row:", row_temp)
+            row_bikes = row[2]
+            #print("Available bikes for this row:", row_bikes)
+            row_time = row[8]
+            #print("Time in row: ", row_time)
+            temp_weight = 1/(math.sqrt((row_temp - current_temp)**2) + 0.5)
+            time_weight = 1/(math.sqrt((round((row_time - current_time).total_seconds()/60)**2)) + 0.5)
+            weight = temp_weight + time_weight 
+            #print("Weight:", weight)
+            weight_total += weight
+            #print("Total of weights:", weight_total)
+            weighted_predictor = row_bikes * weight
+            #print("Available bikes (weighted):", weighted_bikes)
+            weighted_predictors_total += weighted_bikes
+            #print("Weighted bike total so far:", weighted_bikes_total)
+        prediction = round(weighted_predictors_total/weight_total)
+        print("Available bikes at station", station_no, "prediction:", prediction)
+    print("Done.")
 
 
 
