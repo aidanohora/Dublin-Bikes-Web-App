4c82cf0f0539a42d2a75c68a0ac083b02521b866
Aidan <aidan.ohora@ucdconnect.ie>
2019-03-28 18:25:29 +0000

    updated to access new RDS instance

--- nearest_neighbours.py
+++ nearest_neighbours.py
@@ -3,14 +3,14 @@ import math
 import datetime
 
 mydb = mysql.connector.connect(
-    host="host",
-    user="user",
-    passwd="password"
+    host="newdublinbikesinstance.cevl8km57x9m.us-east-1.rds.amazonaws.com",
+    user="root",
+    passwd="secretpass"
 )
 
 cursor = mydb.cursor(buffered=True)
 
-cursor.execute("SELECT DISTINCT station_no FROM dbbikes.station_fixed ORDER BY station_no")
+cursor.execute("SELECT DISTINCT station_no FROM innodb.station_fixed ORDER BY station_no")
 
 station_no_rows = cursor.fetchall()
 
@@ -19,7 +19,7 @@ station_nos = []
 for i in station_no_rows:
     station_nos.append(i[0])
 
-cursor.execute("SELECT * FROM dbbikes.weather ORDER BY date DESC, time DESC LIMIT 1")
+cursor.execute("SELECT * FROM innodb.weather ORDER BY date DESC, time DESC LIMIT 1")
 
 weather_row = cursor.fetchone()
 
@@ -40,15 +40,15 @@ current_day = weather_row[3].weekday()
 #if weekday, use only weekday records:
 if (current_day < 5):
     for station_no in station_nos:
-        #cursor.execute("SELECT available_bikes FROM dbbikes.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
+        #cursor.execute("SELECT available_bikes FROM innodb.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
         #current_bikes = cursor.fetchone()
         #print("Current bikes at station no.", station_no, "is:", current_bikes)
-        cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) < 5" % (station_no, current_weather))
+        cursor.execute("SELECT DISTINCT * FROM innodb.station_var JOIN innodb.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) < 5" % (station_no, current_weather))
         rows = cursor.fetchall()
         #print(rows)
         if rows == []: #if a new weather is encountered, use records for all weather
             #print("New weather encountered:", current_weather)
-            cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' weekday(weather.date) < 5" % station_no)
+            cursor.execute("SELECT DISTINCT * FROM innodb.station_var JOIN innodb.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' weekday(weather.date) < 5" % station_no)
             rows = cursor.fetchall()
         weight_total = 0 
         weighted_predictors_total = 0  
@@ -76,15 +76,15 @@ if (current_day < 5):
 #if weekend, use only records from that day:
 else:
     for station_no in station_nos:
-        #cursor.execute("SELECT available_bikes FROM dbbikes.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
+        #cursor.execute("SELECT available_bikes FROM innodb.station_var WHERE station_no = %s ORDER BY last_update_date DESC, lat_update_time DESC LIMIT 1" % station_no)
         #current_bikes = cursor.fetchone()
         #print("Current bikes at station no.", station_no, "is:", current_bikes)
-        cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) = %s" % (station_no, current_weather, current_day))
+        cursor.execute("SELECT DISTINCT * FROM innodb.station_var JOIN innodb.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weather.description = '%s' AND weekday(weather.date) = %s" % (station_no, current_weather, current_day))
         rows = cursor.fetchall()
         #print(rows)
         if rows == []: #if a new weather is encountered, use records for all weather
             #print("New weather encountered:", current_weather)
-            cursor.execute("SELECT DISTINCT * FROM dbbikes.station_var JOIN dbbikes.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weekday(weather.date) = %s" % (station_no, current_day))
+            cursor.execute("SELECT DISTINCT * FROM innodb.station_var JOIN innodb.weather on (station_var.last_update_date = weather.date AND minute(timediff(station_var.lat_update_time, weather.time)) < 11 AND hour(timediff(station_var.lat_update_time, weather.time)) = 0) WHERE station_var.station_no = %s AND station_var.status = 'OPEN' AND weekday(weather.date) = %s" % (station_no, current_day))
             rows = cursor.fetchall()
         weight_total = 0 
         weighted_predictors_total = 0  
