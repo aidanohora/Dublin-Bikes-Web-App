bb13d2c5c5a4006f8fcc777612d1da06e69ca15a
Miguel Martinez <miguelitalia@hotmail.com>
2019-04-09 10:10:16 +0100

    fixed bug with map and markers in separate file for dbikes projecct

--- app.py
+++ app.py
@@ -25,53 +25,53 @@ def index():
 
 def do_work():
     final=[]
-    with open('Dublin Bike New.txt', 'a') as outfile:
-        data = requests.get(
-            "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
-        keep_keys = set()
-        for d in data:
-            for key, value in d.items():
-                if value is True or value is False:
-                    keep_keys.add(key)
-        remove_keys = keep_keys
-        for d in data:
-            for k in remove_keys:
-                del d[k]
-        for d in data:
-            for key, value in d.items():
-                if key == "number":
-                    number = d[key]
-                elif key == "contract_name":
-                    name = d[key]
-                elif key == "address":
-                    address = d[key]
-                    address = address.replace("'", "`")
-                elif key == 'position':
-                    lat = d[key]['lat']
-                    long = d[key]['lng']
-                elif key == 'bike_stands':
-                    bike_stands = d[key]
-                elif key == 'status':
-                    status = d[key]
-                elif key == 'available_bike_stands':
-                    available_bike_stands = d[key]
-                elif key == 'available_bikes':
-                    available_bikes = d[key]
-                elif key == 'last_update':
-                    dt = d[key]
-                    dt = int(dt)
-                    dt = dt / 1000
-                    date = datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S')
-                    date, time = date.split(" ")
-            d = [ [k,v] for k, v in d.items() ] 
-            print(d)
-            final.append(d)
-            last1=str(final)
-            last2=last1+';'
-            print(d,"\n")
+    data = requests.get(
+        "https://api.jcdecaux.com/vls/v1/stations?contract=dublin&apiKey=e4ee2f3aa32f04bfd04c9efea73fef8a4b2b5535").json()
+    keep_keys = set()
+    for d in data:
+        for key, value in d.items():
+            if value is True or value is False:
+                keep_keys.add(key)
+    remove_keys = keep_keys
+    for d in data:
+        for k in remove_keys:
+            del d[k]
+    for d in data:
+        for key, value in d.items():
+            if key == "number":
+                number = d[key]
+            elif key == "contract_name":
+                name = d[key]
+            elif key == "address":
+                address = d[key]
+                address = address.replace("'", "`")
+            elif key == 'position':
+                lat = d[key]['lat']
+                long = d[key]['lng']
+            elif key == 'bike_stands':
+                bike_stands = d[key]
+            elif key == 'status':
+                status = d[key]
+            elif key == 'available_bike_stands':
+                available_bike_stands = d[key]
+            elif key == 'available_bikes':
+                available_bikes = d[key]
+            elif key == 'last_update':
+                dt = d[key]
+                dt = int(dt)
+                dt = dt / 1000
+                date = datetime.utcfromtimestamp(dt).strftime('%Y-%m-%d %H:%M:%S')
+                date, time = date.split(" ")
+        d = [ [k,v] for k, v in d.items() ]
+        print(d)
+        final.append(d)
+        last1=str(final)
+        last2=last1+';'
+        print(d,"\n")
                         
-    print(last2)
+    # print(last2)
     return last2
+
 
 
 if __name__ == '__main__':
--- static/map.js
+++ static/map.js
@@ -1,129 +1,130 @@
-function initMap(data){
-var locations = data;
-var map = new google.maps.Map(document.getElementById("map"), {
-center: new google.maps.LatLng(53.3483, -6.26665,13),
-zoom: 13,
-mapTypeId: 'roadmap',
-mapTypeControl: true,
-      mapTypeControlOptions: {
-          style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
-          position: google.maps.ControlPosition.TOP_CENTER
+function initMap(){
+    var locations = data;
+
+    var map = new google.maps.Map(document.getElementById("map"), {
+    center: new google.maps.LatLng(53.3483, -6.26665,13),
+    zoom: 13,
+    mapTypeId: 'roadmap',
+    mapTypeControl: true,
+          mapTypeControlOptions: {
+              style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+              position: google.maps.ControlPosition.TOP_CENTER
+          },
+          zoomControl: true,
+          zoomControlOptions: {
+              position: google.maps.ControlPosition.LEFT_CENTER
+          },
+          scaleControl: true,
+          streetViewControl: true,
+          streetViewControlOptions: {
+              position: google.maps.ControlPosition.LEFT_TOP
+          },
+          fullscreenControl: true,
+          styles: [
+      {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+      {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+      {
+        featureType: 'administrative.locality',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
       },
-      zoomControl: true,
-      zoomControlOptions: {
-          position: google.maps.ControlPosition.LEFT_CENTER
+      {
+        featureType: 'poi',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
       },
-      scaleControl: true,
-      streetViewControl: true,
-      streetViewControlOptions: {
-          position: google.maps.ControlPosition.LEFT_TOP
+      {
+        featureType: 'poi.park',
+        elementType: 'geometry',
+        stylers: [{color: '#263c3f'}]
       },
-      fullscreenControl: true,
-      styles: [
-  {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
-  {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
-  {
-    featureType: 'administrative.locality',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'poi',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'geometry',
-    stylers: [{color: '#263c3f'}]
-  },
-  {
-    featureType: 'poi.park',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#6b9a76'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry',
-    stylers: [{color: '#38414e'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#212a37'}]
-  },
-  {
-    featureType: 'road',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#9ca5b3'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry',
-    stylers: [{color: '#746855'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'geometry.stroke',
-    stylers: [{color: '#1f2835'}]
-  },
-  {
-    featureType: 'road.highway',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#f3d19c'}]
-  },
-  {
-    featureType: 'transit',
-    elementType: 'geometry',
-    stylers: [{color: '#2f3948'}]
-  },
-  {
-    featureType: 'transit.station',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#d59563'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'geometry',
-    stylers: [{color: '#17263c'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.fill',
-    stylers: [{color: '#515c6d'}]
-  },
-  {
-    featureType: 'water',
-    elementType: 'labels.text.stroke',
-    stylers: [{color: '#17263c'}]
-  }
-]
+      {
+        featureType: 'poi.park',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#6b9a76'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry',
+        stylers: [{color: '#38414e'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#212a37'}]
+      },
+      {
+        featureType: 'road',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#9ca5b3'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry',
+        stylers: [{color: '#746855'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'geometry.stroke',
+        stylers: [{color: '#1f2835'}]
+      },
+      {
+        featureType: 'road.highway',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#f3d19c'}]
+      },
+      {
+        featureType: 'transit',
+        elementType: 'geometry',
+        stylers: [{color: '#2f3948'}]
+      },
+      {
+        featureType: 'transit.station',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#d59563'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'geometry',
+        stylers: [{color: '#17263c'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.fill',
+        stylers: [{color: '#515c6d'}]
+      },
+      {
+        featureType: 'water',
+        elementType: 'labels.text.stroke',
+        stylers: [{color: '#17263c'}]
+      }
+    ]
 
-});
+    });
 
-var infowindow = new google.maps.InfoWindow();
+    var infowindow = new google.maps.InfoWindow();
 
-var marker, i;
+    var marker, i;
 
-for (i = 0; i < locations.length; i++) {
-  marker = new google.maps.Marker({
-    position: new google.maps.LatLng(locations[i][4][1]['lat'],locations[i][4][1]['lng']),
-    map: map,
-    scaleControl: true,
-    draggable: false,
-    animation:google.maps.Animation.DROP
-  });
+    for (i = 0; i < locations.length; i++) {
+      marker = new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][4][1]['lat'],locations[i][4][1]['lng']),
+        map: map,
+        scaleControl: true,
+        draggable: false,
+        animation:google.maps.Animation.DROP
+      });
 
-  google.maps.event.addListener(marker, 'click', (function(marker, i) {
-    return function() {
-      infowindow.setContent('<p class="stationName">'+locations[i][2][1]+'</p>'
-      +'<p class="stationStats">Status: '+locations[i][8][1]+'</p>'
-      +'<p class="stationStats">Available Stands: '+locations[i][6][1]+'</p>'
-      +'<p class="stationStats">Available Bikes: '+locations[i][7][1]+'</p>');
-      infowindow.open(map, marker);
+      google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent('<p class="stationName">'+locations[i][2][1]+'</p>'
+          +'<p class="stationStats">Status: '+locations[i][8][1]+'</p>'
+          +'<p class="stationStats">Available Stands: '+locations[i][6][1]+'</p>'
+          +'<p class="stationStats">Available Bikes: '+locations[i][7][1]+'</p>');
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+
     }
-  })(marker, i));
-
-}
-}
+    }
