5c0e7108702487f73f37badc0aee2d1f720fe7eb
Miguel Martinez <miguelitalia@hotmail.com>
2019-04-09 20:36:44 +0100

    added second graph and made minor styling additions and changes to index.html

--- static/chart.js
+++ static/chart.js
@@ -1,5 +1,6 @@
-    var ctx = document.getElementById('myChart');
-    var chart = new Chart(ctx, {
+    var ctx1 = document.getElementById('myChart1');
+    var ctx2 = document.getElementById('myChart2');
+    var chart1 = new Chart(ctx1, {
 // The type of chart we want to create
 type: 'line',
 
@@ -9,7 +10,53 @@ data: {
     'November', 'December'],
     datasets: [{
         label: 'Which ever label',
-        backgroundColor: 'gray',
+        backgroundColor: '#1f567c',
+        borderColor: 'black',
+        data: [0, 10, 5, 2, 20, 30, 45, 12, 15, 25, 30, 5]
+    }]
+},
+
+// Configuration options go here
+options: {
+        title: {
+        display: true,
+        text: 'Available bikes at: "name of the station"',
+        fontSize: 35
+    },
+    scales: {
+        xAxes: [{
+            scaleLabel: {
+                display: true,
+                labelString: "What ever we want",
+                fontSize: 35
+            }
+        }],
+        yAxes: [{
+            ticks: {
+                beginAtZero: true,
+                suggestedMax: 50,
+            },
+            scaleLabel: {
+                display: true,
+                labelString: "Available bikes",
+                fontSize: 35
+            }
+        }]
+    }
+}
+});
+
+var chart2 = new Chart(ctx2, {
+// The type of chart we want to create
+type: 'line',
+
+// The data for our dataset
+data: {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October',
+    'November', 'December'],
+    datasets: [{
+        label: 'Which ever label',
+        backgroundColor: '#1f567c',
         borderColor: 'black',
         data: [0, 10, 5, 2, 20, 30, 45, 12, 15, 25, 30, 5]
     }]
